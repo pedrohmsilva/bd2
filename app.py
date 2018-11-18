8ef77f38d010f24f516e90a8b6f24f89943cd81f
Vinícius <viniciuscmgo@gmail.com>
2018-11-18 00:38:47 -0200

    alterar, remover e criar pavilhao

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
 import src.unidade_prisional as unidade_prisional
-from src.pavilhao import ListarPavilhoes, BuscarPavilhoes
+from src.pavilhao import ListarPavilhoes, BuscarPavilhoes, CriarPavilhoes, AlterarPavilhoes, RemoverPavilhoes
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,6 +16,9 @@ api.add_resource(unidade_prisional.Listar, '/unidades/listar')
 api.add_resource(unidade_prisional.Buscar, '/unidades/<int:unidade>')
 api.add_resource(ListarPavilhoes, '/pavilhoes/listar')
 api.add_resource(BuscarPavilhoes, '/pavilhoes/buscar')
+api.add_resource(CriarPavilhoes, '/pavilhoes/criar')
+api.add_resource(AlterarPavilhoes, '/pavilhoes/alterar')
+api.add_resource(RemoverPavilhoes, '/pavilhoes/remover')
 
 if __name__ == "__main__":
 	app.run(debug=True)
